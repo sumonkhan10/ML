db9ae13c12ab92655a60660ebd5c2b30eb72ee3c
Morshedul Arefin Khan <sumonkhan10@gmail.com>
2013-01-08 20:45:32 +0600

    ML(C,C++)

---
+++ listing.cc
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main(void)
+{
+	int var;
+	var=100;
+	cout<<"Value of the variable var=";
+		cout<<var;
+	    cout<<"\n";
+
+}
---
+++ listing0.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(void)
+{
+	int var;
+	var=100;
+	cout<<"Value of the variable var=";
+		cout<<var;
+	    cout<<"\n";
+	    //return 0;
+
+}
