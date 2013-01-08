4fd47d66d8cdb00fdf56c7bd4db766243a90f94a
Morshedul Arefin Khan <sumonkhan10@gmail.com>
2013-01-08 21:46:33 +0600

    Bangla Lekha

---
+++ bangla/bangla.php
@@ -0,0 +1,22 @@
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=SolaimanLipi">
+    <style>
+      body {
+        font-family: 'SolaimanLipi', serif;
+        font-size: 48px;
+      }
+    </style>
+  </head>
+  <body>
+    <div>
+    	স্থায়ী ঠিকানা:<br />
+    	মোরশেদুল আরেফিন খান (সুমন)<br />
+		পিতা: জয়নাল আবেদিন খান( বাদশা)<br />
+		মাতা: জাহানারা খান<br />
+		খান লজ, উপজিলা: ভান্ডারিয়া <br />
+		জিলা: পিরোজপুর, বিভাগ: বরিশাল<br />
+		দেশ: বাংলাদেশ 	
+   </div>
+  </body>
+</html>
